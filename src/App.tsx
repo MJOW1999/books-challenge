e88afcc6fb64f7af84629489c19eeb80c6222530
MJOW1999 <michael.jow.williams@gmail.com>
2023-07-01 19:12:34 +0100

    Pagination logic and disabled styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import Pagination from "./components/Pagination";
 import { sampleShelf } from "./testData";
 import BookShelf from "./components/BookShelf";
+import { Book } from "./types";
 
 function App() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [count, setCount] = useState<number>(0);
+  //const [filters, setFilters] = useState();
+
+  // Pagination stuff
+  const [pageTotal, setPageTotal] = useState<number>(5);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  async function getBooks() {
+    const url = "http://nyx.vima.ekt.gr:3000/api/books";
+    try {
+      const response = await fetch(url, {
+        method: "post",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ page: 1, itemsPerPage: 20, filters: [] }),
+      });
+      const data = await response.json();
+      setBooks(data.books);
+      setCount(data.count);
+      const pageTotalCalc = Math.ceil(data.count / 20);
+      setPageTotal(pageTotalCalc);
+      console.log("called");
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, [currentPage]);
+
   return (
     <div className="App">
       <SearchBar />
-      <BookShelf bookList={sampleShelf} />
-      <Pagination />
+      <p>Showing 20 of {count} books</p>
+      <BookShelf bookList={books} />
+      <Pagination
+        pageTotal={pageTotal}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,14 +1,59 @@
 import React, { useCallback } from "react";
 import { styled } from "styled-components";
 
-const Pagination = () => {
+interface PaginationProps {
+  pageTotal: number;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+}
+
+const Pagination = ({
+  pageTotal,
+  currentPage,
+  setCurrentPage,
+}: PaginationProps) => {
+  console.log(currentPage);
+  const goBack = useCallback(() => {
+    setCurrentPage(currentPage - 1);
+  }, [currentPage, setCurrentPage]);
+
+  const goForward = useCallback(() => {
+    setCurrentPage(currentPage + 1);
+  }, [currentPage, setCurrentPage]);
+
   return (
     <PaginationContainer>
-      <button name="first-page">First</button>
-      <button name="page-one">Previous</button>
-      <p>Current</p>
-      <button name="page-one">Next</button>
-      <button name="last page">Last</button>
+      <MemoLimitButton
+        name="first-page"
+        onClick={() => setCurrentPage(1)}
+        disabled={currentPage === 1}
+      >
+        First
+      </MemoLimitButton>
+      <MemoLimitButton
+        name="previous page"
+        onClick={goBack}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </MemoLimitButton>
+      <p>
+        Page {currentPage} of {pageTotal}
+      </p>
+      <MemoLimitButton
+        name="next page"
+        onClick={goForward}
+        disabled={currentPage === pageTotal}
+      >
+        Next
+      </MemoLimitButton>
+      <MemoLimitButton
+        name="last page"
+        onClick={() => setCurrentPage(pageTotal)}
+        disabled={currentPage === pageTotal}
+      >
+        Last
+      </MemoLimitButton>
     </PaginationContainer>
   );
 };
@@ -18,5 +63,28 @@ const PaginationContainer = styled.section`
   justify-content: space-evenly;
   align-items: center;
 `;
+
+const LimitButton = styled.button<{ disabled: boolean }>`
+  box-sizing: content-box;
+  display: inline-block;
+  border-width: 0px;
+  border-color: #34ace0;
+  height: 30px;
+  border-radius: 1px;
+  margin: 0rem 1rem;
+  background: ${(props) => (props.disabled ? "#dddddd" : "#34ace0")};
+  color: white;
+  margin-left: 4px;
+  margin-right: 4px;
+  font-weight: 100;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "auto")};
+
+  :hover {
+    background: #227093;
+    cursor: pointer;
+  }
+`;
+
+const MemoLimitButton = React.memo(LimitButton);
 
 export default Pagination;
